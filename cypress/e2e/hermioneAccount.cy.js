1ec11652931877aa0caddbc17adb1de2417f835e
Olha Myrutenko <ovka888@gmail.com>
2024-04-28 17:33:42 +0300

    Solution

---
+++ cypress/e2e/hermioneAccount.cy.js
@@ -0,0 +1,72 @@
+const { generateData } = require('./hermioneAccount.spec');
+/// <reference types='cypress' />
+
+describe('Bank app', () => {
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('should provide the ability to work with bank account', () => {
+    const {
+      depositAmount,
+      withdrawAmount,
+      balance,
+      user,
+      accountNumber,
+      balanceAfterWithdrow,
+      formattedDate
+    } = generateData();
+
+    cy.contains('.btn', 'Customer Login').click();
+    cy.get('[name="userSelect"]').select(user);
+    cy.contains('.btn', 'Login').click();
+
+    cy.contains('[ng-hide="noAccount"]', 'Account Number')
+      .contains('strong', accountNumber)
+      .should('be.visible');
+    cy.contains('[ng-hide="noAccount"]', 'Balance')
+      .contains('strong', '5096')
+      .should('be.visible');
+    cy.contains('.ng-binding', 'Dollar')
+      .should('be.visible');
+
+    cy.get('[ng-click="deposit()"]').click();
+    cy.get('[placeholder="amount"]').type(depositAmount);
+    cy.contains('[type="submit"]', 'Deposit').click();
+
+    cy.get('[ng-show="message"]')
+      .should('contain', 'Deposit Successful');
+    cy.contains('[ng-hide="noAccount"]', 'Balance')
+      .contains('strong', balance)
+      .should('be.visible');
+
+    cy.get('[ng-click="withdrawl()"]').click();
+    cy.contains('[type="submit"]', 'Withdraw')
+      .should('be.visible');
+    cy.get('[placeholder="amount"]').type(withdrawAmount);
+    cy.contains('[type="submit"]', 'Withdraw').click();
+
+    cy.get('[ng-show="message"]')
+      .should('contain', 'Transaction successful');
+    cy.contains('[ng-hide="noAccount"]', 'Balance')
+      .contains('strong', balanceAfterWithdrow)
+      .should('be.visible');
+
+    cy.get('[ng-click="transactions()"]').click();
+    cy.get('#start').type(formattedDate);
+
+    cy.get('td.ng-binding').should('contain', depositAmount);
+    cy.get('td.ng-binding').should('contain', 'Credit');
+    cy.get('td.ng-binding').should('contain', withdrawAmount);
+    cy.get('td.ng-binding').should('contain', 'Debit');
+
+    cy.get('[ng-click="back()"]').click();
+
+    cy.get('#accountSelect').select('1002');
+
+    cy.get('[ng-click="transactions()"]').click();
+    cy.get('td.ng-binding').should('not.exist');
+    cy.get('.logout').click();
+    cy.get('#userSelect').should('contain.text', '---Your Name---');
+  });
+});
--- cypress/e2e/hermioneAccount.spec.js
+++ cypress/e2e/hermioneAccount.spec.js
@@ -1,11 +1,30 @@
+import { faker } from '@faker-js/faker';
 /// <reference types='cypress' />
 
-describe('Bank app', () => {
-  before(() => {
-    cy.visit('/');
-  });
+function generateData() {
+  const depositAmount = `${faker.number.int({ min: 500, max: 1000 })}`;
+  const withdrawAmount = `${faker.number.int({ min: 50, max: 500 })}`;
+  const startBalance = 5096;
+  const balance = startBalance + parseInt(depositAmount);
+  const balanceAfterWithdrow = balance - parseInt(withdrawAmount);
+  const user = 'Hermoine Granger';
+  const accountNumber = '1001';
 
-  it('should provide the ability to work with Hermione\'s bank account', () => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  const formattedDate = `${year}-${month}-${day}T00:00`;
 
-  });
-});
+  return {
+    depositAmount,
+    withdrawAmount,
+    balance,
+    user,
+    accountNumber,
+    balanceAfterWithdrow,
+    formattedDate
+  };
+}
+
+module.exports = { generateData };
