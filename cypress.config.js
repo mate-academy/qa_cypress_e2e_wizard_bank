ce93d5680764aa0f3efd46cf3f9b847d86339179
Pavlo Roiev <157385125+SilverF1nger@users.noreply.github.com>
2024-05-13 18:25:52 +0300

    TestsForHermoine

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://www.globalsqa.com/angularJs-protractor/BankingProject/',
+    
+    baseUrl: 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login',
+    
     setupNodeEvents(on, config) {
     }
   }
